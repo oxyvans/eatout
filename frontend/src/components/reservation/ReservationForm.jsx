49140679ecbb2fffc636735771170b2723cc2161
oxyvans <benja.1999.g@hotmail.com>
2022-10-25 18:40:45 -0300

    add style

--- frontend/src/components/reservation/ReservationForm.jsx
+++ frontend/src/components/reservation/ReservationForm.jsx
@@ -37,10 +37,59 @@ const enviarDatos = (event) => {
     Server.Reservation(param , body);   
 }
   return (
+
+    <>
+    <div className="cont_conteiner container">
+    <h2 className='section__title'>Reserve now</h2>
+      <span className='section__subtitle'>When the restaurant confirms your reservation, we will send you an email</span>
+        <form className='cont_form' action="" onSubmit={enviarDatos}>
+            <div className='form-div'>
+                <label className='form-tag'> Name</label>
+                <input type="text"  name='name' onChange={handleInputChangeP} 
+                    className='form-input'  
+                    placeholder='Insert your Name'/>
+            </div>
+
+            <div className='form-div'>
+                <label className='form-tag'>Email</label>
+                <input type="email" name='mail' onChange={handleInputChangeP}
+                    className='form-input'  
+                    placeholder='Insert your Email'/>
+            </div>
+            <div className='form-div'>
+                <label className='form-tag'>Telephone</label>
+                <input type="text" name='tel' onChange={handleInputChangeP}
+                    className='form-input'  
+                    placeholder='Insert your Telephone'/>
+            </div>
+            <div className='form-div'>
+                <label className='form-tag'>Day</label>
+                <input type="date" name='date' onChange={handleInputChangeP}
+                    className='form-input'/>
+            </div>
+            <div className='form-div'>
+                <label className='form-tag'>Time</label>
+                <input  type="time" name='time' onChange={handleInputChangeP}
+                    className='form-input'/>
+            </div>
+            <div className='form-div'>
+                <label className='form-tag'>Guests</label>
+                <input type="number" name='guests' onChange={handleInputChangeP}
+                    className='form-input'  
+                    placeholder='Insert Number of guests'/>
+            </div>
+            <div className="button-l">  
+            <button className='button button--flex'>
+                Reserve <i class="uil uil-calender icno-r"></i>
+            </button>
+            </div>
+        </form>
+    </div>
+
+    {/*
     <div className='reserv' id='reserv'>
       <h2 className='section__title'>Reserve now</h2>
       <span className='section__subtitle'>When the restaurant confirms your reservation, we will send you an email</span>
-
       <form action="" onSubmit={enviarDatos}>
         <input type="text"  name='name' placeholder='nombre' onChange={handleInputChangeP}/>
         <input type="email" name='mail' placeholder='emali' onChange={handleInputChangeP}/>
@@ -51,8 +100,10 @@ const enviarDatos = (event) => {
 
         <button className='button_small'> Reserv</button>
       </form>
-    
+  
     </div>
+    */}
+    </>
   )
 }
 
