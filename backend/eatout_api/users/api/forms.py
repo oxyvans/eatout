4354c7722058087591cdbb339e74b0bd4ff16358
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-10-25 10:50:27 -0300

    changed restName field

--- backend/eatout_api/users/api/forms.py
+++ backend/eatout_api/users/api/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from socket import fromshare
+
 
 class SignUpForm(UserCreationForm):
     username=forms.CharField(max_length=45)
